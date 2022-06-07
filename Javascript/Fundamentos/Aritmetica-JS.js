e5c7e1c5a39a78244a59ea4fbb926952a858d968
Mercy synthesis one <carlos.lopez-mo@uniminuto.edu.co>
2022-06-06 19:19:19 -0500

    Operadores matematicos, presedencia de operadores entre signos se hace primero suma,resta,multiplicacion etc

--- Javascript/Fundamentos/Aritmetica-JS.js
+++ Javascript/Fundamentos/Aritmetica-JS.js
@@ -1,3 +1,42 @@
+//https://www.w3schools.com/js/js_arithmetic.asp
+
+
+
+/*Operadores y operandos
+Los números (en una operación aritmética) se llaman operandos .
+La operación (a realizar entre los dos operandos) está definida por un operador .
+
+numeros son operando 
+signos son el operador
+100 + 50
+*/
+
+/*Presedencia de operadores
+Cuando se usan paréntesis, las operaciones dentro de los paréntesis se calculan primero.
+
+Cuando muchas operaciones tienen la misma precedencia (como la suma y la resta), se calculan de izquierda a derecha:
+
+let x = 100 + 30 * 3;
+console.log(x); resultado 190
+
+let y = (100+30)*3;
+console.log(y);resultado 390
+
+*/
+
+
+/*
+let x = 10;
+x +=5;
+console.log(x);
+
+let x = 10;
+x *=5;
+console.log(x);
+*/
+
+
+
 
 
 
