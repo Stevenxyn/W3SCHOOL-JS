331dadce3ab96460ea72d22326f0e6b65de2d2ea
Mercy synthesis one <carlos.lopez-mo@uniminuto.edu.co>
2022-08-01 23:29:39 -0500

    Exercises codewars #3 and #4

---
+++ Ejercicios/PaginasPractica/codewars/E3.js
@@ -0,0 +1,14 @@
+//Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+}
+
+//solution exercise 
+function makeUpperCase(str) {
+    return str.toUpperCase()
+}
+console.log(makeUpperCase("Aprendiendo Javascript!"));
+
+//solution with arrow function
+const makeUpperCaseTwo = (str) => str.toUpperCase();
+console.log(makeUpperCaseTwo("Solucion desde un arrow function"));
