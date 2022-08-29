af4a9ae05676762f2aae1336208680bb862ceb10
Mercy synthesis one <carlos.lopez-mo@uniminuto.edu.co>
2022-08-28 22:32:33 -0500

    Calculadora imc pro

--- Ejercicios/Ejercicio14-CalcularIMC-DOM/main.js
+++ Ejercicios/Ejercicio14-CalcularIMC-DOM/main.js
@@ -1,30 +1,29 @@
 //Formula para calcular imc IMC = Peso (kg) / altura (m)2
-
-//cuando cargue la pagina llame la funcion
 window.onload = iniciar;
 
 function iniciar() {
-    //recibo el valor del input 
-    var btnCalcularImc = document.getElementById("btnCalcularImc")
-    //escuchar el evento click y nos traiga esa funcion
-    btnCalcularImc.addEventListener('click', clickBtnCalcularImc)
+    //apenas cargue la pagina inicie esto
+    document.getElementById('btnCalcular').onclick = mostrarDatos;
+    document.getElementById('btnBorrar').onclick = borrarDatos;
 }
 
-//funcion que me dara un resultado cuando se oprima el boton
-function clickBtnCalcularImc() {
-    let txtPeso = document.getElementById('txtPeso');
-        var peso = parseInt(txtPeso.value);
-    let txtAltura = document.getElementById('txtAltura');
-        var altura = txtAltura.value;
-    var imc =  peso / Math.pow(altura,2);
-    alert(`Tu Imc es de: ${Math.round(imc)}`)
-}
-//para acceder a una variable hay que llamar la funcion antes
-/*
-clickBtnCalcularImc()
-console.log(nombre);
-*/
+//obtenemos datos y realizamos formula para pintar dichos datos
+function mostrarDatos() {
+    let peso = document.getElementById('txtPeso');
+    peso = peso.value;
+    let altura = document.getElementById('txtAltura');
+    altura = altura.value;
+    //Formula aplicada para calcular IMC
+    const IMC = peso / Math.pow(altura, 2);
+    resultado = `Tú IMC es de ${IMC.toFixed(1)}`
 
+    //pintar datos en en pantalla
+    document.getElementById('cajaResultadoIMC').innerHTML = resultado;
+
+}
+
+//Recargamos pagina y borramos los datos de paso
+const borrarDatos = () => location.reload()
 
 
 
