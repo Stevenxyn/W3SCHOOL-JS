0707cbe8ef3888bcb6081d9ea96d52d3a06d680d
Mercy synthesis one <carlos.lopez-mo@uniminuto.edu.co>
2022-09-04 20:20:56 -0500

    Saludar a usuario con una funcion y un mensaje predeterminado

---
+++ Ejercicios/PaginasPractica/codewars/E12.js
@@ -0,0 +1,11 @@
+/*
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+*/
+
+const saludar = nombre => console.log(`Hola ${nombre} como te encuentras?`);;
+
+saludar("Steven")
